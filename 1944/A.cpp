d3e173f4a832de83673067c29ef3a6c38f6d19df
Nameet <nameetsn@gmail.com>
2024-12-12 22:47:30 +0530

    added folders

---
+++ 1944/A.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+int main(){
+  ll t;
+  cin>>t;
+  while(t--){
+    ll n,k;
+    cin>>n>>k;
+    if(k<n-1) cout<<n<<endl;
+    else cout<<1<<endl;
+  }
+}
