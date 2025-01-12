3162796c4e9c15ac6df04e6bfbed0ade5dd93562
Nameet <nameetsn@gmail.com>
2025-01-12 12:12:48 +0530

    1300-1500

---
+++ by rating/1300-1500/the_bento_box_adventure.cpp
@@ -0,0 +1,13 @@
+//PRACTICING
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+                          
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    ll A=1+2+3+4+5;
+    ll a,b,c,d;
+    cin>>a>>b>>c>>d;
+    cout<<A-(a+b+c+d)<<"\n";
+}
